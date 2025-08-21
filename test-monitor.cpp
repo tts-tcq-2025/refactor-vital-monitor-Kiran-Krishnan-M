27ed6ee3b2e89c43a7d5f168ea36a3dd630ccf0b
Kiran Krishnan M <krishnankiran886@gmail.com>
2025-08-21 13:56:09 +0530

    Update test-monitor.cpp

--- test-monitor.cpp
+++ test-monitor.cpp
@@ -4,13 +4,10 @@
 
 
 TEST(MonitorTest, NotOkWhenAnyVitalIsOffRange) {
-  
-  // Pulse rate is out of range (102)
   EXPECT_NE(checkVitals(99, 102, 70), VitalStatus::OK);
-
-  // All vitals are within range
   EXPECT_EQ(checkVitals(98.1, 70, 98), VitalStatus::OK);
 }
+
 
 
 
