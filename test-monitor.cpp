e1a5b11e055acf76e4065bfa052c80d7534a434b
Kiran Krishnan M <krishnankiran886@gmail.com>
2025-08-21 13:35:05 +0530

    Update test-monitor.cpp

--- test-monitor.cpp
+++ test-monitor.cpp
@@ -2,13 +2,18 @@
 #include <gtest/gtest.h>
 #include "./monitor.h"
 
+extern bool testMode;  // Declare the flag
+
 TEST(MonitorTest, NotOkWhenAnyVitalIsOffRange) {
+
+  testMode = true;  // Disable blinking
   // Pulse rate is out of range (102)
   EXPECT_NE(checkVitals(99, 102, 70), VitalStatus::OK);
 
   // All vitals are within range
   EXPECT_EQ(checkVitals(98.1, 70, 98), VitalStatus::OK);
 }
+
 
 
 
