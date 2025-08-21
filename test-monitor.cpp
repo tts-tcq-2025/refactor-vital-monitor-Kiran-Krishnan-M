d800d90806ce487df06744cd2f550db3a2cc6178
Kiran Krishnan M <krishnankiran886@gmail.com>
2025-08-21 10:29:45 +0530

    Update test-monitor.cpp

--- test-monitor.cpp
+++ test-monitor.cpp
@@ -1,7 +1,22 @@
 #include <gtest/gtest.h>
 #include "./monitor.h"
 
-TEST(Monitor, NotOkWhenAnyVitalIsOffRange) {
-  ASSERT_FALSE(vitalsOk(99, 102, 70));
-  ASSERT_TRUE(vitalsOk(98.1, 70, 98));
+TEST(MonitorTest, DetectsTemperatureOutOfRange) {
+  EXPECT_EQ(checkVitals(103.0, 75.0, 95.0), VitalStatus::TEMPERATURE_CRITICAL);
+  EXPECT_EQ(checkVitals(94.0, 75.0, 95.0), VitalStatus::TEMPERATURE_CRITICAL);
 }
+
+TEST(MonitorTest, DetectsPulseOutOfRange) {
+  EXPECT_EQ(checkVitals(98.0, 55.0, 95.0), VitalStatus::PULSE_CRITICAL);
+  EXPECT_EQ(checkVitals(98.0, 105.0, 95.0), VitalStatus::PULSE_CRITICAL);
+}
+
+TEST(MonitorTest, DetectsSpo2OutOfRange) {
+  EXPECT_EQ(checkVitals(98.0, 75.0, 85.0), VitalStatus::SPO2_CRITICAL);
+}
+
+TEST(MonitorTest, AllVitalsInRange) {
+  EXPECT_EQ(checkVitals(98.6, 75.0, 98.0), VitalStatus::OK);
+}
+
+
