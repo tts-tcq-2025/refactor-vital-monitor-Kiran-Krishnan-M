0f365acb7d8043aca518f5a5f6015beca9b5a67a
Kiran Krishnan M <krishnankiran886@gmail.com>
2025-08-22 09:55:20 +0530

    Update monitor.cpp

--- monitor.cpp
+++ monitor.cpp
@@ -1,72 +1,56 @@
-#include "./monitor.h"
+#include "monitor.h"
 #include <iostream>
 #include <thread>
 #include <chrono>
 #include <unordered_map>
-#include <algorithm>
+#include <limits>
 
 using std::cout, std::flush, std::this_thread::sleep_for, std::chrono::seconds;
 
+// ---------- Pure Functions ----------
 bool isOutOfRange(float value, const VitalThreshold& threshold) {
   return value < threshold.min || value > threshold.max;
 }
 
-struct VitalCheck {
-  float value;
-  VitalThreshold threshold;
-  VitalStatus status;
-};
-
-VitalStatus checkVitals(float temperature, float pulseRate, float spo2) {
-  const VitalCheck checks[] = {
-    {temperature, {95.0, 102.0}, VitalStatus::TEMPERATURE_CRITICAL},
-    {pulseRate,   {60.0, 100.0}, VitalStatus::PULSE_CRITICAL},
-    {spo2,        {90.0, 150.0}, VitalStatus::SPO2_CRITICAL}
-  };
-
-    auto it = std::find_if(std::begin(checks), std::end(checks),
-        [](const VitalCheck& check) {
-            return isOutOfRange(check.value, check.threshold);
-        });
-
-    return (it != std::end(checks)) ? it->status : VitalStatus::OK;
+std::string checkVital(const VitalStatus& vital) {
+  if (isOutOfRange(vital.value, vital.threshold)) {
+    return vital.name + " is out of range!";
+  }
+  return "";
 }
 
-void blinkAlert() {
-  for (int i = 0; i < 6; ++i) {
+// ---------- I/O Functions ----------
+void blinkAlert(const std::string& message) {
+  cout << message << "\n";
+  for (int i = 0; i < 6; i++) {
     cout << "\r* " << flush;
     sleep_for(seconds(1));
     cout << "\r *" << flush;
     sleep_for(seconds(1));
   }
+  cout << "\n";
 }
 
-void printAlertMessage(VitalStatus status) {
-  static const std::unordered_map<VitalStatus, const char*> messages = {
-    {VitalStatus::TEMPERATURE_CRITICAL, "Temperature is critical!\n"},
-    {VitalStatus::PULSE_CRITICAL, "Pulse Rate is out of range!\n"},
-    {VitalStatus::SPO2_CRITICAL, "Oxygen Saturation out of range!\n"}
+// ---------- Monitoring Function ----------
+bool vitalsOk(float temperature, float pulseRate, float spo2) {
+  std::unordered_map<std::string, VitalThreshold> thresholds = {
+      {"Temperature", {95, 102}},
+      {"Pulse Rate", {60, 100}},
+      {"Oxygen Saturation", {90, std::numeric_limits<float>::infinity()}} // only min check
   };
 
-  auto it = messages.find(status);
-  if (it != messages.end()) {
-    cout << it->second;
-  }
-}
-void alert(VitalStatus status) {
-  if (status != VitalStatus::OK) {
-    printAlertMessage(status);
-    blinkAlert();
-  }
-}
-/*
-int main() {
-  float temperature = 103.0;
-  float pulseRate = 85.0;
-  float spo2 = 95.0;
+  VitalStatus vitals[] = {
+      {"Temperature", temperature, thresholds["Temperature"]},
+      {"Pulse Rate", pulseRate, thresholds["Pulse Rate"]},
+      {"Oxygen Saturation", spo2, thresholds["Oxygen Saturation"]}
+  };
 
-  VitalStatus status = checkVitals(temperature, pulseRate, spo2);
-  alert(status);
-
-  return 0;
-} */
+  for (const auto& vital : vitals) {
+    std::string msg = checkVital(vital);
+    if (!msg.empty()) {
+      blinkAlert(msg);
+      return false;
+    }
+  }
+  return true;
+}
